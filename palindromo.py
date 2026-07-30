56396ec05b12ac5d5ee166a85821bd051bc53beb
Marcos Araújo <105182709+Marcos-Arauj@users.noreply.github.com>
2026-07-30 11:54:40 -0300

    Delete palindromo.py

--- palindromo.py
+++
@@ -1,8 +0,0 @@
-frase = input('Digite uma frase: ').lower().strip()
-sem_espacos = frase.replace(' ', '')
-
-
-if sem_espacos == sem_espacos[::-1]:
-    print('É um palíndromo')
-else:
-    print('Não é um palíndromo')
